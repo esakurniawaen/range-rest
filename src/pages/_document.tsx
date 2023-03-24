19a6c624f93ddf753cb0bc5e0ea5608f773d84ef
Esa Kurniawan <esakurniawan866@gmail.com>
2023-03-24 15:40:41 +0800

    Moved all icons into the icons folder

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
         <Html lang="en">
             <Head>
                 <link rel="manifest" href="/manifest.json" />
-                <link rel="apple-touch-icon" href="/icon.png"></link>
+                <link rel="apple-touch-icon" href="/icons/icon.png"></link>
             </Head>
             <body className="bg-slate-50 text-slate-500 dark:bg-slate-900 dark:text-slate-400">
                 <Main />
