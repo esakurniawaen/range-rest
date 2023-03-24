cdf533e3ed216ce6411ce175e0a0606900caad48
Esa Kurniawan <esakurniawan866@gmail.com>
2023-03-24 13:38:10 +0800

    Added PWA

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
     return (
         <Html lang="en">
-            <Head />
+            <Head>
+                <link rel="manifest" href="/manifest.json" />
+                <link rel="apple-touch-icon" href="/icon.png"></link>
+            </Head>
             <body className="bg-slate-50 text-slate-500 dark:bg-slate-900 dark:text-slate-400">
                 <Main />
                 <NextScript />
